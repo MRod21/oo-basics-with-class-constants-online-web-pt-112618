27cb30b661ac270174ef4c0df29d2be97ff9e570
Mario Rodriguez <mario.rodriguez35@yahoo.com>
2018-12-16 03:01:02 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -6,7 +6,9 @@ class Shoe
 
   def initialize(brand)
     @brand = brand
-    if !BRANDS.include?(@brand)
+    if BRANDS.include?(@brand)
+      BRANDS.clear
+    else
     BRANDS << brand
     end
   end
